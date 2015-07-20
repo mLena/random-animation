2ccd575aad7e7506244662a684a15668a7220946
mLena <marilena.bucur28@gmail.com>
2015-07-20 17:43:34 +0300

    refactor(javascript): added constants, split logic

--- www/code.js
+++ www/code.js
@@ -1,19 +1,45 @@
+var MINIMUM_WIDTH_HEIGHT = 1;
+var MAXIMUM_WIDTH_HEIGHT = 140;
+var MINIMUM_COORDINATES = 0;
+var MAXIMUM_COORDINATES = 700;
+var MINIMUM_RGB = 0;
+var MAXIMUM_RGB = 255;
+var INTERVAL_MILLISECONDS = 500;
 
-setInterval (function(){ myDiv();}, 500);
-function myDiv(){
-  var min = 1;
-  var max = 140;
-  var minimum = 0;
-  var maximum = 255;
-  var minP = 0;
-  var maxP = 800;
+setInterval (animationInterval, INTERVAL_MILLISECONDS);
+
+function animationInterval(){
   var theElement = document.querySelector("#unique");
-  var r = (Math.floor(Math.random() * (maximum - minimum + 1)) + minimum);
-  var g = (Math.floor(Math.random() * (maximum - minimum + 1)) + minimum);
-  var b = (Math.floor(Math.random() * (maximum - minimum + 1)) + minimum);
-  theElement.style.backgroundColor = 'rgb(' + r + ',' + g + ',' + b + ')';
-  theElement.style.width = (Math.floor(Math.random() * (max - min + 1)) + min) + 'px';
-  theElement.style.height = (Math.floor(Math.random() * (max - min + 1)) + min) + 'px';
-  theElement.style.left = (Math.floor(Math.random() * (maxP - minP + 1)) + minP) + 'px';
-  theElement.style.top = (Math.floor(Math.random() * (maxP - minP + 1)) + minP) + 'px';
+  theElement.style.backgroundColor = randomizeBackgroundColor();
+  theElement.style.width =
+    buildPixelValue(
+      getRandomNumber(MINIMUM_WIDTH_HEIGHT, MAXIMUM_WIDTH_HEIGHT)
+    );
+  theElement.style.height =
+    buildPixelValue(
+      getRandomNumber(MINIMUM_WIDTH_HEIGHT, MAXIMUM_WIDTH_HEIGHT)
+    );
+  theElement.style.left =
+    buildPixelValue(
+      getRandomNumber(MINIMUM_COORDINATES, MAXIMUM_COORDINATES)
+    );
+  theElement.style.top =
+    buildPixelValue(
+      getRandomNumber(MINIMUM_COORDINATES, MAXIMUM_COORDINATES)
+    );
+}
+
+function getRandomNumber(min, max){
+  return (Math.floor(Math.random() * (max - min + 1)) + min);
+}
+
+function randomizeBackgroundColor(){
+  var r = getRandomNumber(MINIMUM_RGB, MAXIMUM_RGB);
+  var g = getRandomNumber(MINIMUM_RGB, MAXIMUM_RGB);
+  var b = getRandomNumber(MINIMUM_RGB, MAXIMUM_RGB);
+  return 'rgb(' + r + ',' + g + ',' + b + ')';
+}
+
+function buildPixelValue(numericValue){
+  return numericValue + 'px';
 }
